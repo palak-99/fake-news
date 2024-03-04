4f3d6de389280b73bf2c2c4e42ac7fca74bf8a5f
Dhananjay Sharma <101014511+dhananjay-sharma04@users.noreply.github.com>
2024-03-05 02:17:56 +0530

    Update routes.py
    few correction

--- .venv/routes.py
+++ .venv/routes.py
@@ -14,8 +14,9 @@ def newsVerify(input_text):
     # padded_encoded_title = pad_sequences([encoded], maxlen=max_length, padding='pre')
     # output = fake_news_dectect_bot.predict(padded_encoded_title)
     # output = np.where(0.4 > output, 1, 0)
+    output = 'Yes this News is fake'
     # if output[0][0] == 1:
     #     print('Yes this News is fake')
     # else:
     #     print('No, It is not fake')
-    return jsonify({"res": input_text}), 200
+    return jsonify({"res": output}), 200
